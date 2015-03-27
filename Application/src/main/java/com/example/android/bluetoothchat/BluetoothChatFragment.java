4ff5185a0cb2c4f351ca58c4d75fa2d7df8334a6
Dustin Jones <drjones@gus.pittstate.edu>
2015-03-27 12:04:51 -0500

    Commit

--- Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
+++ Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
@@ -587,9 +587,11 @@ public class BluetoothChatFragment extends Fragment implements SeekBar.OnSeekBar
                        ((TextView) getView().findViewById(R.id.r2temp)).setText(" "+readMessage.charAt(3)+readMessage.charAt(4));
                        ((TextView) getView().findViewById(R.id.r3temp)).setText(" "+readMessage.charAt(5)+readMessage.charAt(6));
                        ((TextView) getView().findViewById(R.id.r4temp)).setText(" "+readMessage.charAt(7)+readMessage.charAt(8));
-                       // ((TextView) getView().findViewById(R.id.day)).setText(" "+readMessage.charAt(9));
-                     //   ((TextView) getView().findViewById(R.id.schednum)).setText(" "+readMessage.charAt(9));
-                       // ((TextView) getView().findViewById(R.id.time)).setText(" "+readMessage.charAt(10)+readMessage.charAt(11)+readMessage.charAt(12)+readMessage.charAt(13));
+                    ((TextView) getView().findViewById(R.id.textView16)).setText("Room 1: "+readMessage.charAt(10)+readMessage.charAt(11)+" and "+readMessage.charAt(23)+readMessage.charAt(24)+" degrees");
+                    ((TextView) getView().findViewById(R.id.textView2)).setText("Room 2: "+readMessage.charAt(12)+readMessage.charAt(13)+" and "+readMessage.charAt(25)+readMessage.charAt(26)+" degrees");
+                    ((TextView) getView().findViewById(R.id.textView18)).setText("Room 3: "+readMessage.charAt(14)+readMessage.charAt(15)+" and "+readMessage.charAt(27)+readMessage.charAt(28)+" degrees");
+                    ((TextView) getView().findViewById(R.id.textView20)).setText("Room 4: "+readMessage.charAt(16)+readMessage.charAt(17)+" and "+readMessage.charAt(29)+readMessage.charAt(30)+" degrees");
+                    ((TextView) getView().findViewById(R.id.textView7)).setText("Scheduled Event Times: "+readMessage.charAt(18)+readMessage.charAt(19)+":"+readMessage.charAt(20)+readMessage.charAt(21)+" and "+readMessage.charAt(31)+readMessage.charAt(32)+":"+readMessage.charAt(33)+readMessage.charAt(34));
                     break;
                 case Constants.MESSAGE_DEVICE_NAME:
                     // save the connected device's name
